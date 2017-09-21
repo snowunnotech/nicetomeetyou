0f1760a62142f1d60b3efa893b2ea048ecbc0622
zjs <jinsheng358@gmail.com>
2017-09-21 22:29:36 +0800

    Use django signals to handle post_save
    Add django signals to handle post_save. This will be triggered when a
    new model object has been saved.

---
+++ news/signals.py
@@ -0,0 +1,9 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from news.models import News
+
+
+@receiver(post_save, sender=News)
+def news_updated_post_save(sender, **kwargs):
+    print('Saved: {}'.format(kwargs['instance'].__dict__))
