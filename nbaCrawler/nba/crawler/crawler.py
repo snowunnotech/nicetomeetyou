11eed0e07bbab4348a52f888dbbdf0e393cd95b5
JerryTsai <mit0301@gmail.com>
2018-03-27 10:26:04 +0800

    modify get news function

--- nbaCrawler/nba/crawler/crawler.py
+++ nbaCrawler/nba/crawler/crawler.py
@@ -1,25 +1,35 @@
 from bs4 import BeautifulSoup
-from nba.models import News
 import requests
 
-url = "https://nba.udn.com"
 
-res = requests.get(url + "/nba/index?gr=www")
+def getNews():
+    url = "https://nba.udn.com"
 
-soup = BeautifulSoup(res.text, 'html.parser')
+    res = requests.get(url + "/nba/index?gr=www")
 
-for news in soup.select("#news_body")[0].select("dt"):
-    title = news.select("h3")[0].text
-    content_url = url + news.select("a")[0].attrs.get("href")
-
-    res = requests.get(content_url)
     soup = BeautifulSoup(res.text, 'html.parser')
 
-    content = ""
-    for p in soup.select("#story_body_content")[0].select("p"):
-        if "facebooktwitterpinterest" not in p.text:
-            content += p.text
+    ret = []
 
-    print(title)
-    print(content)
-    print(content_url)
+    for news in soup.select("#news_body")[0].select("dt"):
+        title = news.select("h3")[0].text
+        content_url = url + news.select("a")[0].attrs.get("href")
+
+        res = requests.get(content_url)
+        soup = BeautifulSoup(res.text, 'html.parser')
+
+        content = ""
+        for p in soup.select("#story_body_content")[0].select("p"):
+            if "facebooktwitterpinterest" not in p.text:
+                content += p.text
+
+        news = {
+            'title' : title,
+            'content' : content,
+            'url' : url,
+        }
+
+        ret.append(news)
+
+    return ret
+
