c410acdda884d596a9e725479d32740b8eaac983
CJHwong <cjhwong@gmail.com>
2018-04-10 05:33:04 +0800

    First commit with a working spider

---
+++ udn/udn/spiders/news_spider.py
@@ -0,0 +1,33 @@
+import scrapy
+
+from udn.items import NewsItem
+
+
+class NewsSpider(scrapy.Spider):
+    name = 'news'
+    allowed_domains = ['nba.udn.com']
+    start_urls = ('', )
+
+    def start_requests(self):
+        urls = [
+            'https://nba.udn.com/nba/index?gr=www',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        urls = response.xpath('//div[@id="news_body"]/dl/dt/a/@href').extract()
+        for url in urls:
+            yield scrapy.Request('https://nba.udn.com' + url, callback=self.parse_post)
+
+    def parse_post(self, response):
+        item = NewsItem()
+        item['title'] = response.xpath(
+            '//div[@id="story_body_content"]/h1/text()').extract_first()
+        item['image_url'] = response.xpath(
+            '//div[@id="story_body_content"]//span//figure//a/img/@data-src').extract_first()
+        item['content'] = ''.join(response.xpath(
+            '//div[@id="story_body_content"]//p/text()').extract())
+        item['url'] = response.url
+
+        yield item
