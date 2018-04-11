f18931730a13e912a7210116281b3f6a1e1e9c90
CJHwong <cjhwong@gmail.com>
2018-04-11 21:04:58 +0800

    Finished web page with Vue.js

--- Unnotech/web/news/urls.py
+++ Unnotech/web/news/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.views.generic.base import TemplateView
 from news import views
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^news/$', views.news_list),
     url(r'^news/(?P<article_id>[0-9]+)/$', views.news_detail),
 ]
