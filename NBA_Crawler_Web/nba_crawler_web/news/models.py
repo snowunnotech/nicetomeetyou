ed9bbbef1167fdd988e02c108aa3367aca18f27d
zondayang <u226699@gmail.com>
2018-07-23 23:20:53 +0800

    Create Database

--- NBA_Crawler_Web/nba_crawler_web/news/models.py
+++ NBA_Crawler_Web/nba_crawler_web/news/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    author = models.CharField(max_length=50)
+    datetime = models.DateTimeField()
+    title = models.CharField(max_length=100)
+    image_url = models.CharField(max_length=250, blank=True, default="")
+    story_url = models.CharField(max_length=250)
+    content = models.TextField()
+    video_url = models.CharField(max_length=250, blank=True, default="")
+
+    class Meta:
+        unique_together = (("datetime", "title"),)
