db2454c056a4a99b43100d76be807514ba4d2d0b
CTY <joker1618@livemail.tw>
2018-08-10 16:21:28 +0800

    crawl with ORM

--- crawl.py
+++ crawl.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
 import requests
-import psycopg2
+
+from main.models import News
+
 
 HOST = 'https://nba.udn.com'
 URL = HOST + '/nba/index?gr=www'
@@ -24,85 +26,41 @@ def get_web_page(url):
         return None
 
 
-def connect_db():
-    '''
-    Get the connection to the postgreSQL.
-    '''
-    conn = psycopg2.connect(
-        database="dg62hgin80pv3",
-        user="mblkytpskwbsjl",
-        password="8959ea54deffcbda363077049518915120a33eb2efb5677091a379a1604b6068",
-        host="ec2-54-225-76-201.compute-1.amazonaws.com",
-        port="5432"
-    )
-    return conn
-
-
-def is_new(conn, dt):
-    '''
-    Check if the news has already existed in the database.
-    '''
-    # get urls of news
-    href = dt.a['href']
-    url = HOST + href
-
-    # check if the news exists
-    cur = conn.cursor()
-    cur.execute('SELECT url FROM main_news;')
-    old_newses = cur.fetchall()
-    if url in [old_news[0] for old_news in old_newses]:
-        return False
-    return url
-
-
-def crawl_and_save(conn, url):
+def create_and_save(url):
     '''
     Crawl the detail page of news, clean the data and save to DB.
     '''
     soup = get_web_page(url)
-    title = soup.find("h1", "story_art_title").text
-    pub_time = soup.find("div", "shareBar__info--author").span.text
-    pub_time = datetime.strptime(pub_time, '%Y-%m-%d %H:%M')
-    crawl_time = datetime.now()
-    paragraphs = soup.find("div", id="story_body_content").find_all("span")[2].find_all("p")
+    # check if the news exists
+    if News.objects.is_new(url=url):
+        title = soup.find("h1", "story_art_title").text
+        pub_time = soup.find("div", "shareBar__info--author").span.text
+        pub_time = datetime.strptime(pub_time, '%Y-%m-%d %H:%M')
+        crawl_time = datetime.now()
+        paragraphs = soup.find("div", id="story_body_content").find_all("span")[2].find_all("p")
 
-    # clean the content data
-    content = ''
-    for paragraph in paragraphs:
-        if paragraph.find("figure"):
-            continue
-        content += paragraph.text
+        # clean the content data
+        content = ''
+        for paragraph in paragraphs:
+            if paragraph.find("figure"):
+                continue
+            content += paragraph.text
 
-    # save to database
-    cur = conn.cursor()
-    cur.execute('''INSERT INTO main_news (
-        title,
-        url,
-        pub_time,
-        crawl_time,
-        content) VALUES (%s, %s, %s, %s, %s)''', (
-        title,
-        url,
-        pub_time,
-        crawl_time,
-        content,
-    ))
-    conn.commit()
+        News.objects.create(
+            url=url,
+            title=title,
+            pub_time=pub_time,
+            crawl_time=crawl_time,
+            content=content
+        )
 
 
-def main():
+def crawl():
     '''
     Find the target urls, and run the crawling process.
     '''
-    conn = connect_db()
     soup = get_web_page(URL)
     dts = soup.find("div", id="news").find_all("dt")
     for dt in dts:
-        url = is_new(conn, dt)
-        if url:
-            crawl_and_save(conn=conn, url=url)
-    conn.close()
-
-
-if __name__ == '__main__':
-    main()
+        href = dt.a['href']
+        create_and_save(url=HOST+href)
--- main/models.py
+++ main/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from django.urls import reverse
+from django.db.models.signals import post_save
+
+
+class NewsManager(models.Manager):
+    def is_new(self, url=None):
+        qs = self.get_queryset().filter(url=url)
+        if qs.exist():
+            return False
+        return True
 
 
 class News(models.Model):
@@ -9,11 +18,22 @@ class News(models.Model):
     title = models.CharField(max_length=200, null=True, blank=True)
     content = models.TextField(null=True, blank=True)
 
+    objects = NewsManager()
+
     class Meta:
         ordering = ['-pub_time', ]
 
     def __str__(self):
-        return self.title
+        if self.title:
+            return self.title
+        return str(self.id)
 
     def get_absolute_url(self):
         return reverse('news_detail', kwargs={'pk': self.id})
+
+
+def post_save_trigger(sender, instance, created, *args, **kwargs):
+    pass
+
+
+post_save.connect(post_save_trigger, sender=News)
