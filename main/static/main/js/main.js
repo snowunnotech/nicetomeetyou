3f5db552bfc58d0edd1859844cee1086b0333553
CTY <joker1618@livemail.tw>
2018-08-10 17:18:03 +0800

    adjust frontend: open new tab

--- main/static/main/js/main.js
+++ main/static/main/js/main.js
@@ -12,7 +12,8 @@ $(document).ready(function() {
                 // Clear the home page and add detail information of news.
                 var text1 = "<h3>" + data.title + "</h3><br><br>"
                 var text2 = "<p>" + data.content + "</p>"
-                var text3 = "<p>原文網址：<a href='" + data.url + "'>" + data.url + "</a></p>"
+                var text3 = "<p>原文網址：<a target='_blank' rel='noopener noreferrer' href='"
+                            + data.url + "'>" + data.url + "</a></p>"
                 var btn = "<a class='mt-10 ml-auto btn btn-primary' href='/'>回到新聞列表</a>"
                 $('.detail').append(text1, text2, text3, btn);
                 $('.cards').remove();
