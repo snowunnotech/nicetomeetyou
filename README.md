73a7ea1ad8fc6cc9190d3e8666fd12301eec85db
Dopiz <r.j.liu0903@gmail.com>
2018-09-25 19:12:08 +0800

    Auto Update New Posts

--- README.md
+++ README.md
@@ -2,15 +2,15 @@
 - \[x] 抓取 https://nba.udn.com/nba/index?gr=www 中的焦點新聞。
 - \[x] 使用 [Django](https://www.djangoproject.com/) 設計恰當的 Model，并將所抓取新聞存儲至 DB。
 - \[x] 使用 [Django REST Framework](http://www.django-rest-framework.org/) 配合 AJAX 實現以下頁面：
-	 * 焦點新聞列表
-	 * 新聞詳情頁面
-- \[ ] 以 Pull-Request 的方式將代碼提交。
+	- 焦點新聞列表
+	- 新聞詳情頁面
+- \[x] 以 Pull-Request 的方式將代碼提交。
 	
 ## 進階要求
 - \[x] 實現爬蟲自動定時抓取。
-	`利用 django-q 在 Scheduled tasks 裡增加任務排程。`
-- \[ ] 每當抓取到新的新聞時立即通知頁面。
-	`try django websocket`
+	<br> `利用 django-q 在 Scheduled tasks 裡增加任務排程。`
+- \[x] 每當抓取到新的新聞時立即通知頁面。
+	<br> `目前能夠每十秒監測資料庫中是否有新的資料，有新資料則即時插入前端顯示。`
 - \[ ] 將本 demo 部署至服務器並可正確運行。
 
 
@@ -25,3 +25,6 @@ djangorestframework
 markdown
 django-filter
 ```
+
+## Screenshot
+![image](https://i.imgur.com/qvAEttl.jpg)
--- nbaCrawler/static/main.js
+++ nbaCrawler/static/main.js
@@ -1,39 +1,60 @@
 var url = 'http://127.0.0.1:8000/api/nbaCrawler/'
+var dataLength = 0
 
-$.ajax({
-    method: 'GET',
-    url: url,
-    success: function(data) {
-        console.log(data)
-        renderHTML(data);
-        console.log("Success")
-    },
-    error: function(error_data) {
-        console.log("Error")
+$(document).ready(function(){
+    $firstload = loadPost();
+
+    if($firstload) {
+        setInterval('loadPost()', 10000);
     }
+
+
 })
 
+function loadPost() {
+    $.ajax({
+        method: 'GET',
+        url: url,
+        success: function(data) {
+            renderHTML(data);
+            console.log("Success")
+        },
+        error: function(error_data) {
+            console.log("Error")
+        }
+    })
+
+    return true
+}
 
 
 function renderHTML(data) {
     var container = document.getElementById("container")
     var htmlString = ""
 
-    for (i = 0; i < data.length; i++) {
-        htmlString += "<tr><th><img src=" + data[i].post_image_url + " width='220px'></th>";
-        htmlString += "<th>" + data[i].post_title + "</th>";
-        var d = new Date(data[i].post_date)
+    if (data.length > dataLength) {
+        
+        console.log('Add New Post.')
 
-        var time = d.getFullYear() + "-" + ("0" + (d.getMonth()+1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2) 
-                        + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
+        for (i = 0; i < data.length - dataLength; i++) {
+            htmlString += "<tr><th><img src=" + data[i].post_image_url + " width='220px'></th>";
+            htmlString += "<th>" + data[i].post_title + "</th>";
+            var d = new Date(data[i].post_date)
+    
+            var time = d.getFullYear() + "-" + ("0" + (d.getMonth()+1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2) 
+                            + " " + ("0" + d.getHours()).slice(-2) + ":" + ("0" + d.getMinutes()).slice(-2);
+    
+            htmlString += "<th>" + time + "</th>";
+            htmlString += "<th><a href=" + data[i].post_url + ">Link</a></th>"
+            htmlString += "<th><input type='button' id='btn' onclick='loadContent(" + data[i].pk + ")' value='Content' /></th></tr>"
+    
+        }
 
-        htmlString += "<th>" + time + "</th>";
-        htmlString += "<th><a href=" + data[i].post_url + ">Link</a></th>"
-        htmlString += "<th><input type='button' id='btn' onclick='loadContent(" + data[i].pk + ")' value='Content' /></th></tr>"
-        // htmlString += "<th><input type='button' id='btn' id=" + data[i].pk + "/></th></tr>"
+        container.insertAdjacentHTML('afterbegin', htmlString);
     }
 
-    container.insertAdjacentHTML('beforeend', htmlString);
+    dataLength = data.length
+
 }
 
 function loadContent(post_id) {
@@ -50,12 +71,13 @@ function loadContent(post_id) {
     })
 
     function renderHTML(data) {
+        
         var container = document.getElementById("content")
         var htmlString = ""
 
-        htmlString += "<dialog open id='content_dialog'><div style='width: 80%'><button onclick='closeDialog()' style='align: left' class='btn'>X</button><br>"
-        htmlString += "<img src=" + data.post_image_url + " width='500'>";
-        htmlString += "<div style='text-align: left;'>" + data.post_content + "</div></div></dialog>"
+        htmlString += "<dialog open id='content_dialog'><div><button onclick='closeDialog()' style='align: left' class='btn'>X</button><br>"
+        htmlString += "<img src=" + data.post_image_url + " width='500px'>";
+        htmlString += "<div style='text-align: left; max-height: 400px; overflow:auto;'>" + data.post_content + "</div></div></dialog>"
 
         container.insertAdjacentHTML('beforeend', htmlString);   
     }
