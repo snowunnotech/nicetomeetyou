785e18f14f845111f722835793dbfaf1954f80ea
Yihong <h308011@gmail.com>
2018-09-30 19:01:46 +0800

    Create Django App

---
+++ app/views.py
@@ -0,0 +1,17 @@
+"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(request,
+        'app/index.html',
+        {
+            'year':datetime.now().year,
+        })
---
+++ nicetomeetyou/urls.py
@@ -0,0 +1,12 @@
+"""
+Definition of urls for nicetomeetyou.
+"""
+
+from datetime import datetime
+from django.urls import path
+import app.views
+
+
+urlpatterns = [
+    path('',app.views.home,name='home'),
+    ]
