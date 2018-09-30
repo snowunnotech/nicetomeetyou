78f604f22606a98020d951f4546a6a12724203af
Yihong <h308011@gmail.com>
2018-09-30 19:25:01 +0800

    1.實現爬蟲自動定時抓取

--- manage.py
+++ manage.py
@@ -1,10 +1,25 @@
-#!/usr/bin/env python
 """
 Command-line utility for administrative tasks.
 """
-
 import os
 import sys
+import threading
+import time
+import socket
+import requests
+
+def circlecrawler():
+  time.sleep(5)
+  while 1==1:     
+      r = requests.get('https://localhost:'+str(sys.argv[-1]) +'/api/getclecrawler' )
+      print(r.text)
+      time.sleep(60*5)
+          
+number = sys.argv[-1].isdigit()
+if number:
+    t = threading.Thread(target = circlecrawler)
+    t.start()
+
 
 if __name__ == "__main__":
     os.environ.setdefault(
