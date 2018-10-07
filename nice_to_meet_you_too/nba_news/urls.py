2338f24f432e9971c6d81e09685aff3570eef808
r04943148@ntu.edu.tw <lky880154@yahoo.com.tw>
2018-10-08 02:39:44 +0800

    Update ajax

--- nice_to_meet_you_too/nba_news/urls.py
+++ nice_to_meet_you_too/nba_news/urls.py
@@ -8,6 +8,9 @@ router.register(r'nba_news', views.NbaNewsViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^home$', views.homepage),
-    url(r'^story/(?P<news_id>[0-9]+)', views.storypage)
+    url(r'^story/(?P<news_id>[0-9]+)', views.storypage),
+    url(r'^crawl$', views.crawl)
+    #url(r'nba_news/$', views.NbaNewsList.as_view()),
+    #url(r'nba_news/(?P<pk>[0-9]+)/$', views.NbaNewsDetail.as_view())
 ]
 
--- nice_to_meet_you_too/nba_news/views.py
+++ nice_to_meet_you_too/nba_news/views.py
@@ -1,18 +1,37 @@
 from django.shortcuts import render
+from django.forms.models import model_to_dict
 from rest_framework import viewsets
+from rest_framework import mixins
+from rest_framework import generics
 from nba_news.models import NbaNews
 from nba_news.serializers import NbaNewsSerializer
-from django.shortcuts import render
-from django.forms.models import model_to_dict
 from datetime import datetime
 import subprocess
+from django.http import HttpResponse
+
+class NbaNewsList(mixins.ListModelMixin,
+                  generics.GenericAPIView):
+    queryset = NbaNews.objects.all().order_by("-created")
+    serializer_class = NbaNewsSerializer
+    
+    def get(self, request, *args, **kwargs):
+        subprocess.run('python manage.py crawl_nba'.split())
+        return self.list(request, *args, **kwargs)
+    
+class NbaNewsDetail(mixins.RetrieveModelMixin,
+                    generics.GenericAPIView):
+    queryset = NbaNews.objects.all().order_by("-created")
+    serializer_class = NbaNewsSerializer
+    
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
 
 class NbaNewsViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = NbaNews.objects.all()
+    queryset = NbaNews.objects.all().order_by("-created")
     serializer_class = NbaNewsSerializer
 
 def homepage(request):
-    subprocess.run('python manage.py crawl_nba'.split())
     return render(request, 'index.html')
 
 def storypage(request, news_id):
@@ -21,3 +40,7 @@ def storypage(request, news_id):
     dict_render['created'] = 'Today'
     return render(request, 'story.html', dict_render)
 
+def crawl(request):
+    subprocess.run('python manage.py crawl_nba'.split())
+    response = HttpResponse("Finsh crawling")
+    return response
