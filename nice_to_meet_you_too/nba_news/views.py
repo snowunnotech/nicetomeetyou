d2976bf9eb41321ab42a1f398000b77fd49ec4a9
r04943148@ntu.edu.tw <lky880154@yahoo.com.tw>
2018-10-07 20:47:01 +0800

    Update storypage

--- nice_to_meet_you_too/nba_news/views.py
+++ nice_to_meet_you_too/nba_news/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 from nba_news.models import NbaNews
 from nba_news.serializers import NbaNewsSerializer
 from django.shortcuts import render
+from django.forms.models import model_to_dict
 
 class NbaNewsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = NbaNews.objects.all()
@@ -12,5 +13,8 @@ def homepage(request):
     return render(request, 'index.html')
 
 def storypage(request, news_id):
-    return render(request, 'story.html', {'news_id': news_id})
+    news = NbaNews.objects.get(id = news_id)
+    dict_render = model_to_dict(news)
+    dict_render['created'] = 'Today'
+    return render(request, 'story.html', dict_render)
 
