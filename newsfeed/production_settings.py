1d3ca2b85a78766fc7e1f1a9a5bbab19a0a7ae69
Wei-Lin Ku <hughku@gmail.com>
2018-10-08 23:10:34 +0800

    Fix static files path

--- newsfeed/production_settings.py
+++ newsfeed/production_settings.py
@@ -135,11 +135,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(STATIC_BASE_DIR, "static")
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(STATIC_BASE_DIR, 'static'),
 )
