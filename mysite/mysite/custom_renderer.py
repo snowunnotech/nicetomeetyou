a6bbcdcba60104de140897956a235e38bcff9851
polar2031 <polar2031@gmail.com>
2018-11-09 17:38:19 +0800

    remove untracked file

--- mysite/mysite/custom_renderer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import renderers
-
-
-class PlainTextRenderer(renderers.BaseRenderer):
-    media_type = 'text/plain'
-    format = None
-
-    def render(self, data, media_type=None, renderer_context=None):
-        return data.encode(self.charset)
