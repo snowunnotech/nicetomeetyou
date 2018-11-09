5433c45c5d536e4375ce73b916926396318faebd
polar2031 <polar2031@gmail.com>
2018-11-09 16:34:18 +0800

    add setup

---
+++ mysite/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nicetomeetyou',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Kuan-Lun Chen',
+      author_email='kchen1@umassd.edu',
+      packages=[''],
+     )
