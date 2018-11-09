11b14580ac526ef2a351d241bd912d6eec72dfd0
Muzque <zerosky1943@gmail.com>
2018-11-09 18:37:49 +0800

    add celery-log and detach beat

--- celery/run.sh
+++ celery/run.sh
@@ -1,3 +1,3 @@
 python wait_rabbitmq.py
-celery -A celery_app beat
-celery -A celery_app worker -l info
+celery -A celery_app beat -f log/celery_beat.log --detach
+celery -A celery_app worker -l info -f log/celery_worker.log
