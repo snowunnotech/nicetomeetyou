f0008ed4b9528e21e5c6c5473d1a94305ccf6812
j129008 <j129008@gmail.com>
2019-02-21 16:59:29 +0000

    set primary key

--- nba/news/models.py
+++ nba/news/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class News(models.Model):
     title = models.TextField()
-    link = models.TextField()
+    link = models.TextField(primary_key=True)
 
     class Meta:
         db_table = "news"
