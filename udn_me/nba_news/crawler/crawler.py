4f732a0041f0879c97e2409dde9ab23de14888c1
PinChia Chen <chenargar@gmail.com>
2019-06-02 14:47:08 +0800

    Add files via upload
    fix bug

--- udn_me/nba_news/crawler/crawler.py
+++ udn_me/nba_news/crawler/crawler.py
@@ -24,10 +24,10 @@ def news_crawler(pages: int = 10):
     url_base = 'https://nba.udn.com'
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0'}
 
-    # 抓取詳細新聞頁面的URL
+    # 從焦點新聞首頁抓取詳細新聞頁面的URL，預設抓取10頁
     page_detail_url = []
     for i in range(pages):
-        res = requests.get(url = f'https://nba.udn.com/nba/cate/6754/-1/newest/({pages}+1)', headers = headers)
+        res = requests.get(url = 'https://nba.udn.com/nba/cate/6754/-1/newest/%d'%(i+1), headers = headers)
         soup = BeautifulSoup(res.text, features='lxml')
         news_list_body = soup.find(id = 'news_list_body')
         for ele in news_list_body.find_all('a'):
@@ -44,6 +44,7 @@ def news_crawler(pages: int = 10):
         post_source_url = page_url
 
         try:
+            # 如果抓取的新聞ID已存在，則跳出此層迴圈(為了避免抓取重複新聞)
             if Post.objects.filter(id=post_id).exists():
                 continue
 
@@ -66,6 +67,7 @@ def news_crawler(pages: int = 10):
             for ele in content[1::]:
                 post_content += ele.text
 
+            # 依表格規格建立相關資料
             post_list.append(Post(
                 id = post_id,
                 title = post_title,
@@ -81,6 +83,7 @@ def news_crawler(pages: int = 10):
             print(e)
             continue
 
+    # 將資料庫表格加入大量資料
     Post.objects.bulk_create(post_list)
 
     print('-----Crawler End-----')
