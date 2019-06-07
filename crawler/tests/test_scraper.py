7fa727a1875f64b0f7e54e082b33606b03aff60d
stanleyuan <stanley830912@gmail.com>
2019-06-07 22:33:11 +0800

    test: add test for request new detail

--- crawler/tests/test_scraper.py
+++ crawler/tests/test_scraper.py
@@ -3,33 +3,37 @@
 import unittest
 
 from crawler.scraper import Scraper
-from crawler.constant import URL
+from crawler.constant import URL, TEST_URL
 
 
 class ScraperTestCase(unittest.TestCase):
 
     def setUp(self):
+        pass
+
+    def test_scraper(self):
         self.scraper = Scraper(URL)
         self.scraper.create_request_headers()
         self.scraper.create_request_data()
         self.scraper.request()
         self.scraper.get_response_text()
 
-    def test_create_request_data(self):
+        hot_news = "焦點新聞"
 
         self.assertTrue(self.scraper.request_data)
-
-    def test_create_request_headers(self):
-
         self.assertTrue(self.scraper.request_headers)
-
-    def test_request(self):
-
         self.assertTrue(self.scraper.response)
-
-    def test_get_response_text(self):
-
         self.assertTrue(self.scraper.text)
-
         # check get the right content from the web
-        self.assertIn("焦點新聞", self.scraper.text)
+        self.assertIn(hot_news, self.scraper.text)
+
+    def test_get_response_text_of_news_detail(self):
+        self.scraper_detail = Scraper(TEST_URL)
+        self.scraper_detail.create_request_headers()
+        self.scraper_detail.create_request_data()
+        self.scraper_detail.request()
+        self.scraper_detail.get_response_text()
+
+        news_title = "穿雷納德球衣「加持」？ 加拿大高球好手抓鳥失利"
+
+        self.assertIn(news_title, self.scraper_detail.text)
