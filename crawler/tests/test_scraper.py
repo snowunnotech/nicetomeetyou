ad8f42729c5a29fe64ff4a366bb2431af0855319
stanleyuan <stanley830912@gmail.com>
2019-06-07 23:04:05 +0800

    refactor: rename constant URL to BASE_URL

--- crawler/tests/test_scraper.py
+++ crawler/tests/test_scraper.py
@@ -3,7 +3,7 @@
 import unittest
 
 from crawler.scraper import Scraper
-from crawler.constant import URL, TEST_URL
+from crawler.constant import BASE_URL, TEST_URL, PROJECT_ROOT
 
 
 class ScraperTestCase(unittest.TestCase):
@@ -12,7 +12,10 @@ class ScraperTestCase(unittest.TestCase):
         pass
 
     def test_scraper(self):
-        self.scraper = Scraper(URL)
+        '''
+        test for scrap for website
+        '''
+        self.scraper = Scraper(BASE_URL)
         self.scraper.create_request_headers()
         self.scraper.create_request_data()
         self.scraper.request()
@@ -28,12 +31,15 @@ class ScraperTestCase(unittest.TestCase):
         self.assertIn(hot_news, self.scraper.text)
 
     def test_get_response_text_of_news_detail(self):
-        self.scraper_detail = Scraper(TEST_URL)
-        self.scraper_detail.create_request_headers()
-        self.scraper_detail.create_request_data()
-        self.scraper_detail.request()
-        self.scraper_detail.get_response_text()
+        '''
+        test for scrap for detail website
+        '''
+        self.scraper = Scraper(TEST_URL)
+        self.scraper.create_request_headers()
+        self.scraper.create_request_data()
+        self.scraper.request()
+        self.scraper.get_response_text()
 
         news_title = "穿雷納德球衣「加持」？ 加拿大高球好手抓鳥失利"
 
-        self.assertIn(news_title, self.scraper_detail.text)
+        self.assertIn(news_title, self.scraper.text)
