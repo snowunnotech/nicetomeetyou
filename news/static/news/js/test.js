dbbc6a82182daf90e916c352f0da29dd3c1df1d4
stanleyuan <stanley830912@gmail.com>
2019-06-08 20:41:17 +0800

    feat: add news detail site

--- news/static/news/js/test.js
+++ news/static/news/js/test.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         html = '<div class="box"><a href="https://youtu.be/s6zR2T9vn2c" class="image fit"><img src="images/pic01.jpg" alt="" /></a><div class="inner">';
         html += '<h3>' + data[i]["title"] + '</h3>';
         html += '<p>'+ data[i]["created_at"] + '</p><a href="'
-        html += 'http://127.0.0.1:8000/api/news/' + data[i]['id']
+        html += 'http://127.0.0.1:8000/news/detail/' + data[i]['id']
         html += '" class="button fit">Read</a></div>';
         $(".thumbnails").append(html);
       }
--- news/views.py
+++ news/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def index(request, template='news/index.html'):
 
     return render(request, template)
+
+def detail(request, pk, template='news/detail.html'):
+
+    context = {
+        'pk': pk
+    }
+
+    return render(request, template, context)
