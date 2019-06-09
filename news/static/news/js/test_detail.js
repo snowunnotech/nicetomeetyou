2ef6aeca5ebd22cc59fe2c76f98ce91521cbd650
stanleyuan <stanley830912@gmail.com>
2019-06-09 18:33:39 +0800

    feat: ajax request accroding to host url

--- news/static/news/js/test_detail.js
+++ news/static/news/js/test_detail.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   console.log($("#pk").text());
   $.ajax({
-    url: "http://localhost:8000/api/news/" + $("#pk").text(),
+    url: "http://" + $(".host-url").text() + "/api/news/" + $("#pk").text(),
     dataType: "json",
     success: function(data) {
       var i, html;
