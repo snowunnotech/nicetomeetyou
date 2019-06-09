03f3d8f9bd0c8666c442edf02cd0b1a2343f3680
stanleyuan <stanley830912@gmail.com>
2019-06-09 18:55:42 +0800

    feat: use 404 pages

--- nba/settings.py
+++ nba/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nwti4&#eo-@gljq-$txi)0_q6m5iluwao)8p4%upexib+b3uqs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- news/urls.py
+++ news/urls.py
@@ -4,6 +4,8 @@ from news import views
 
 app_name = 'news'
 
+handler404 = 'news.views.handler404'
+
 urlpatterns = [
     path(r'index/', views.index, name='index'),
     path(r'detail/<int:pk>/', views.detail, name='index'),
