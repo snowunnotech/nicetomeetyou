bcc1a1ac085934dbc886339767ae2eec052f6bba
OHua <OHua@users.noreply.github.com>
2019-06-29 13:33:23 +0800

    Crawl the news in the mainpage Display it in index.html

--- crawlnews/main/views.py
+++ crawlnews/main/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup
+
 
 # Create your views here.
 
 def homepage(request):
 
-    content = {}
+    response = requests.get("https://nba.udn.com/nba/index?gr=www")
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    a_tags = soup.select("div#news_body dl > dt > a")
+
+    content = {
+        "news" : [],
+    }
+    
+    for a_tag in a_tags :
+        news_dictionary = dict( zip(
+            ["time", "title", "statement"], a_tag.stripped_strings
+        ) )
+        content["news"].append(news_dictionary)
+    
 
     return render(request, "index.html", content)
