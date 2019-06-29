1c2f46712d5bb7b71d0820862ef6f92181578399
OHua <OHua@users.noreply.github.com>
2019-06-29 17:29:04 +0800

    Create News models in main app Crawl the detail page and store into db

--- crawlnews/main/views.py
+++ crawlnews/main/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
+
+# import tools
 import requests
 from bs4 import BeautifulSoup
+
+# import class from models
+from .models import News
+
 
 
 # Create your views here.
 
 def homepage(request):
 
-    response = requests.get("https://nba.udn.com/nba/index?gr=www")
+    # do the crawl
+    host = "https://nba.udn.com"
+    response = requests.get( host + "/nba/index?gr=www")
     html = response.text
     soup = BeautifulSoup(html, "html.parser")
     a_tags = soup.select("div#news_body dl > dt > a")
@@ -15,12 +23,35 @@ def homepage(request):
     content = {
         "news" : [],
     }
-    
+
+    # store into db
     for a_tag in a_tags :
+
+        # get everything first
         news_dictionary = dict( zip(
             ["time", "title", "statement"], a_tag.stripped_strings
         ) )
-        content["news"].append(news_dictionary)
-    
+        url_detail = a_tag.get("href")
+        url_id = url_detail.split("/")[-1]
+        image = a_tag.select_one("span.img-boxs img").get("src")
+        # crawl another detail page
+        Soup = BeautifulSoup( requests.get( host + url_detail ).text )
+        author = Soup.select_one("div#story_body_content div.shareBar__info--author")
+        author_dictionary = dict( zip(
+            ["author_time", "author"], author.stripped_strings
+        ) )
+        p_tags = Soup.select("div#story_body_content p:not(:has(div)):not(:empty)")
+        body_content = "\n\n".join( map( lambda x: x.text, p_tags ) )
+        
+        # create model and save it
+        news, created = News.objects.get_or_create(url_id=url_id)
+        if created :
+            news.url_detail = url_detail
+            news.image = image
+            news.__dict__.update(news_dictionary)
+            news.__dict__.update(author_dictionary)        
+            news.body_content = body_content
+            news.save()
+        content["news"].append(news_dictionary)        
 
     return render(request, "index.html", content)
