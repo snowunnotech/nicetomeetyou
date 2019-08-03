52fdafd213d6e772f95e79031808770df219d542
Train Chen <train.chen@vpon.com>
2019-08-03 22:50:07 +0800

    Add parse function for class UdnNbaSpider, parsing title, url

--- mysite/scrapy_code/udn_nba/udn_nba/spiders/udn_nba_spider.py
+++ mysite/scrapy_code/udn_nba/udn_nba/spiders/udn_nba_spider.py
@@ -1,4 +1,6 @@
 from scrapy import Spider
+from scrapy.selector import Selector
+from udn_nba.items import UdnNbaItem
 
 class UdnNbaSpider(Spider):
     name = "udn_nba"
@@ -6,6 +8,18 @@ class UdnNbaSpider(Spider):
     start_urls = ["https://nba.udn.com/nba/index?gr=www"]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)    
+        focus_news = Selector(response)\
+                .xpath("//div[@id='news_body' and @class='box_body']/dl/dt")
+        for news in focus_news:
+            item = UdnNbaItem()
+            item["title"] = news.xpath("a/h3/text()").extract()[0]
+            print(item["title"])
+            item["url"] = news.xpath("a[contains(@href, '/nba/story')]/@href")\
+                .extract()[0]
+            yield item
+
+        #filename = response.url.split("/")[-2] + '.html'
+        #with open(filename, 'wb') as f:
+        #    f.write(response.body)
+
+#//div[@id="news_body"]
