ea6681a83472c4d196a4dd8627929d800b6f2538
ciyang <jason21131@gmail.com>
2019-08-11 23:20:59 +0800

    Add the schedule function and fix the raw query to ORM.

--- NBA_crawler/settings.py
+++ NBA_crawler/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'crawlerApp'
+    'crawlerApp',
+    'django_apscheduler'
 ]
 
 MIDDLEWARE = [
