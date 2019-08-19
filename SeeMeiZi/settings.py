855c047e4ba689b61eaac8a45c09e9dfac27db06
phhuang102i <jesushuang10927@gmail.com>
2019-08-19 23:32:03 +0800

    heroku push

--- SeeMeiZi/settings.py
+++ SeeMeiZi/settings.py
@@ -21,7 +21,8 @@ sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*+$is_2vdr_!vi-q=u62++8=gfrcy4b*!zr5c(ykohr_*q8o0)'
+#SECRET_KEY = '*+$is_2vdr_!vi-q=u62++8=gfrcy4b*!zr5c(ykohr_*q8o0)'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '*+$is_2vdr_!vi-q=u62++8=gfrcy4b*!zr5c(ykohr_*q8o0)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_apscheduler',
     'rest_framework',
     'index',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,4 +121,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+SCRAPY_DIR = os.path.dirname(os.path.dirname('./try_nba'))
+SCRAPY_ROOT = os.path.join(BASE_DIR, 'SeeMeiSpider')
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
--- SeeMeiZi/urls.py
+++ SeeMeiZi/urls.py
@@ -13,21 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.urls import path
 from django.contrib import admin
-from index.views import FeedsView, Homepage, Get_News_List
+from index.views import FeedsView, Homepage, Get_News_List, Detailpage
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'news', Get_News_List, 'news')
+
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^news/$', Homepage),
-    url(r'^newsapi$', Get_News_List.as_view()),
+    url(r'^hotnews/$', Homepage),
+    url(r'^api/', include(router.urls)),
 ]
 urlpatterns += [
-    path('', RedirectView.as_view(url='/news/', permanent=True)),
+    path('', RedirectView.as_view(url='/hotnews/', permanent=True)),
+]
+urlpatterns += [
+    path(r'hotnews/<int:pk>',Detailpage),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
