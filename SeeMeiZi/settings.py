68f288cd797eb043ca955d735fb2f805f51d2fd2
phhuang102i <jesushuang10927@gmail.com>
2019-08-20 00:41:36 +0800

    update host

--- SeeMeiZi/settings.py
+++ SeeMeiZi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '*+$is_2vdr_!vi-q=u62++8=gfrcy4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nbanews20190819.herokuapp.com/']
 
 # Application definition
 
