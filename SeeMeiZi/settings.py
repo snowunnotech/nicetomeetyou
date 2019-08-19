39764e21e9e029f00b2355308f5316a22b312400
phhuang102i <jesushuang10927@gmail.com>
2019-08-19 23:51:26 +0800

    for heroku alter DB setting

--- SeeMeiZi/settings.py
+++ SeeMeiZi/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'SeeMeiZi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
