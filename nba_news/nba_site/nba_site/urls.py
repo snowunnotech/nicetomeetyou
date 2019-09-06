005b5904ab6933576ad33a02c0343ff4700a4059
Fish <myps64152002@yahoo.com.tw>
2019-09-06 21:16:02 +0800

    admin page and title page created

--- nba_news/nba_site/nba_site/urls.py
+++ nba_news/nba_site/nba_site/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
+# from django.urls import path
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+
+from django.conf.urls import url
 from django.urls import path
+from django.contrib import admin
+from news.views import hello_view
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^hello/', hello_view),
 ]
--- nba_news/nba_site/news/views.py
+++ nba_news/nba_site/news/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def hello_view(request):
+    return render(request, 'nba_news.html', {
+        'data': "Hello Django",
+    })
