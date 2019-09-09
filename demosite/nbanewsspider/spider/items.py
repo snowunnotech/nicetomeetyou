584bed6ae2f9d109f6464def0837407ec53dc419
施華桐 <gundambox.shi@gmail.com>
2019-09-09 11:04:39 +0800

    implment scrapy spider to crawl nba news

---
+++ demosite/nbanewsspider/spider/items.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy_djangoitem import DjangoItem
+from nbanewsreader.models import News
+
+class NewsItem(DjangoItem):
+    django_model = News
