aeb9c8d03c339ebd30471f4ca45634f2bbd673cb
phacus <kos147258@gmail.com>
2019-09-27 00:05:23 +0800

    add model meta ordering by pub_time

--- news/models.py
+++ news/models.py
@@ -6,3 +6,6 @@ class Articles(models.Model):
     title = models.TextField()
     content = models.TextField()
     pub_time = models.DateTimeField()
+
+    class Meta:
+        ordering = ['-pub_time']
