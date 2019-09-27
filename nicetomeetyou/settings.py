aa1dc66b36c758babc7f6366cf9056fbf0cf5d6a
phacus <kos147258@gmail.com>
2019-09-28 00:12:21 +0800

    Finish ajax loading content.

--- nicetomeetyou/settings.py
+++ nicetomeetyou/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%)mt_q^l_i)^q)8t80u^*p&mcpxi1ij-*ia*p^sscniy0)ub+e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.56.87']
 
 
 # Application definition
