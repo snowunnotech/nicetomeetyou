74c173a478fd017cae709965158b5e3425dfdc2d
unknown <a5932016@gmail.com>
2019-10-18 16:42:52 +0800

    make it better

--- NbaScrapy/NbaScrapy/NbaScrapy/settings.py
+++ NbaScrapy/NbaScrapy/NbaScrapy/settings.py
@@ -88,3 +88,11 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+import os
+import sys
+import django
+
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ".."))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'nba.settings'
+django.setup()
