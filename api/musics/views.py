0a23dfa6753d77ac3a0e3df93755d1df0f16ecd1
erik_wu <erik_wu@trendmicro.com>
2019-12-25 12:35:09 +0800

    remove detail route

--- api/musics/views.py
+++ api/musics/views.py
@@ -7,7 +7,9 @@ from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
-from rest_framework.decorators import detail_route, list_route
+from rest_framework.decorators import list_route
+from rest_framework.decorators import action
+
 
 
 # Create your views here.
@@ -18,7 +20,7 @@ class MusicViewSet(viewsets.ModelViewSet):
     parser_classes = (JSONParser,)
 
     # /api/music/{pk}/detail/
-    @detail_route(methods=['get'])
+    @action(methods=['get'])
     def detail(self, request, pk=None):
         music = get_object_or_404(Music, pk=pk)
         result = {
