55a98fbb402fd174eab54fcb79c2e9c25e6bfab6
JunChi-Li <61586711+JunChi-Li@users.noreply.github.com>
2020-02-29 01:03:37 +0800

    Add files via upload

---
+++ django_rest/production_settings.py
@@ -0,0 +1,12 @@
+import dj_database_url
+from .settings import * 
+
+DATABASES = {
+    'default': dj_database_url.config(),
+}
+STATIC_ROOT = 'staticfiles' 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') 
+ALLOWED_HOSTS = ['*'] 
+# DEBUG = False 
+
+
