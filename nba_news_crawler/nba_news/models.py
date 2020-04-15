24a4fc5cc58879883098854d267ff3463ff7ccf0
kaze_ <neko123876201@gmail.com>
2020-04-15 17:38:01 +0800

    建立 model

--- nba_news_crawler/nba_news/models.py
+++ nba_news_crawler/nba_news/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class NbaNews(models.Model):
+    url = models.URLField(blank=True)
+    image = models.URLField(blank=True)
+    title = models.CharField(max_length=50)
+    timestamp = models.DateField(auto_now_add=True)
